ec2af6d34c45b14eab0faf55e75a085189275124
Shirley1-Huang <Shirley1_Huang@asus.com>
2025-02-17 10:29:12 +0800

    Update student_assignment.py

--- student_assignment.py
+++ student_assignment.py
@@ -7,7 +7,14 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    loader = PyPDFLoader(q1_pdf)
+    docs = loader.load()
+    #print(docs)
+    
+    text_splitter = CharacterTextSplitter(chunk_overlap  = 0)
+    split_text = text_splitter.split_documents(docs)
+    
+    return split_text[len(split_text)-1]
 
 def hw02_2(q2_pdf):
     pass
