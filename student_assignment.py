37512ceac7941ae462437b8a699b5fba5677f46f
Shirley1-Huang <Shirley1_Huang@asus.com>
2025-02-17 10:32:15 +0800

    Update student_assignment.py

--- student_assignment.py
+++ student_assignment.py
@@ -17,4 +17,30 @@ def hw02_1(q1_pdf):
     return split_text[len(split_text)-1]
 
 def hw02_2(q2_pdf):
-    pass
+    loader = PyPDFLoader(q2_pdf)
+    docs = loader.load()
+    #print(docs)
+    
+    full_contents = ""
+    for content in docs:
+        #print(content.page_content)
+        #print("---------------")
+        full_contents += content.page_content
+        
+    #print(full_contents)
+
+
+    text_splitter = RecursiveCharacterTextSplitter (
+        separators=[r"第\s.*?章\s.|第\s\d.*?條\s?."],
+        chunk_size = 10,
+        chunk_overlap  = 0,
+        is_separator_regex=True
+    )
+    split_text = text_splitter.split_text(full_contents)
+    
+    #print(split_text[110])
+    return len(split_text)
+
+
+#print(hw02_1(q1_pdf))
+#print(hw02_2(q2_pdf))
